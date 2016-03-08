e827a08c9232678f615456c6f92f9a1c417f396e
cynosures <cynosurech@gmail.com>
2016-03-08 16:27:53 +0000

    reduce function

---
+++ how-to-reduce-arrays.js
@@ -0,0 +1,7 @@
+var items = [{price: 10}, {price: 120}, {price: 1000}];
+var reducer = function add(sumSoFar, item){
+  return sumSoFar + item.price;
+};
+
+var total = items.reduce(reducer, 0);
+console.log(total);
